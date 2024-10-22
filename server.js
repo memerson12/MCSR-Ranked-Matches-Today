5109c8733fd66b8e52eab30728cd3e645338a27a
Michael <47831816+memerson12@users.noreply.github.com>
2024-10-21 23:18:51 -0700

    tracing

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
+import "./tracing.cjs";
+
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 import matchesRouter from "./routes/matches.js";
 import axolotlsRouter from "./routes/axolotls.js";
 import worldRecordsRouter from "./routes/world_records.js";
+import { trace } from "@opentelemetry/api";
+
+const tracer = trace.getTracer("mcsr-stats");
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,6 +17,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  trace.getActiveSpan()?.updateName(`${req.method} ${req.path}`);
+  next();
+});
 
 app.use("/api/matches", matchesRouter);
 app.use("/api/axolotls", axolotlsRouter);
